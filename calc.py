6ae331f0b9ae4b593c852f68a3e67719c4dcdb06
GH2654 <dsasdfa!@>
2025-07-27 16:58:37 +0300

    Revert "add numbers"
    This reverts commit 496fbba6d1948a5c2441b7cf86cfe27c22ca7f2f.

--- calc.py
+++ calc.py
@@ -5,12 +5,4 @@ def add_numbers(x, y):
     """
     return x + y
 
-def subtract_numbers(x, y):
-    """
-    This function subtracts the second number from the first and returns the result.
-    """
-    return x - y
-
-print(subtract_numbers(5, 3))    # Output: 2
-
 print(add_numbers(5, 3))    # Output: 8
